3ca6a5454521180ad89215dcb98373ca85826650
Rafael Flores <r.flores@levelup.gt>
2022-12-21 18:55:23 -0600

    Add API structure

---
+++ api.py
@@ -0,0 +1,15 @@
+# API - stateless
+# request
+# URL - dominio - https://google.com - https://189.205.65.69
+# URL - path - https://google.com/photos
+# método - GET - POST - PUT - DELETE 
+# body - {
+#   "name": "Rafael",
+#   "email": "email@ejemplo.com"
+# }
+# headers - Authorization: abc-123, Content-Type: application/jpg, Accept: application/excel
+# Path parameters /photos/1
+# Query params /friends?gender=male&adults=yes
+
+# endpoint 
+# response
